e52a1ce043b0764b82ff3e91991755c2213e4e8a
Franz Hoepfner <kinnarr@hollywuuud.de>
2022-05-15 22:06:58 +0200

    feat: adapt logparser to new format, also save an incrementing line number to instrumentation data

--- analyse/logparser.py
+++ analyse/logparser.py
@@ -8,8 +8,9 @@ import pathlib
 
 run_header_regex = re.compile(r'Open5GS daemon v.+')
 log_line_regex = re.compile(r'(?P<timestamp>[0-9]{2}\/[0-9]{2} [0-9]{2}\:[0-9]{2}\:[0-9]{2}\.[0-9]{3})\: \[(?P<domain>.+?)\] (?P<level>[A-Z]+)\: (?P<message>.+?) \((?P<location>.+\:[0-9]+)\)\n')
-state_data_regex = re.compile(r'\[state\]\{(?P<csv>(.*?\,)+.*?)\}(?P<version>\{.*\})?')
-time_data_regex = re.compile(r'\[time\]\{(?P<csv>(.*?\,)+.*?)\}(?P<version>\{.*\})?')
+state_data_regex = re.compile(r'\[state\]\{(?P<csv>(.*?\,)+.*?)\}([\{\(](?P<version>.*)[\}\)])?')
+time_data_regex = re.compile(r'\[time\]\{(?P<csv>(.*?\,)+.*?)\}([\{\(](?P<version>.*)[\}\)])?')
+timemarker_data_regex = re.compile(r'\[timemarker\]\{(?P<csv>(.*?\,)+.*?)\}([\{\(](?P<version>.*)[\}\)])?')
 
 
 def parse_open5gs_timestamp(timestamp_str: str) -> datetime.datetime:
@@ -32,7 +33,7 @@ def read_log_last_run(logfile: pathlib.Path, dump=False, run=None) -> Generator[
                 if last_was_head and (rematch := log_line_regex.match(line)):
                     timestamp = parse_open5gs_timestamp(rematch.group('timestamp'))
                     if dump:
-                        click.echo(f"Possible run: {timestamp.strftime('%Y%m%d-%H%M%S')}")
+                        click.echo(f"Possible run: {timestamp.strftime('%Y%m%d-%H%M%S.%f')}")
                     else:
                         diff = abs(timestamp - run)
                         if diff < datetime.timedelta(minutes=1):
@@ -60,34 +61,58 @@ def parse_instrumentation_messages(log_generator, instrumentation_data=None) -> 
         instrumentation_data = {'__run_timestamp': None}
 
     first_line = True
+
+    timemarker = {}
+    log_linenumber = 0
+
     for line in log_generator:
         if instrumentation_data['__run_timestamp'] is None:
-            instrumentation_data['__run_timestamp'] = line['timestamp'].strftime('%Y%m%d-%H%M%S')
+            instrumentation_data['__run_timestamp'] = line['timestamp'].strftime('%Y%m%d-%H%M%S.%f')
         elif first_line:
-            run_timestamp = datetime.datetime.strptime(instrumentation_data['__run_timestamp'], '%Y%m%d-%H%M%S')
+            run_timestamp = datetime.datetime.strptime(instrumentation_data['__run_timestamp'], '%Y%m%d-%H%M%S.%f')
             diff = abs(line['timestamp'] - run_timestamp)
             if diff > datetime.timedelta(minutes=1):
                 click.echo(f'Log data not matching run of previous analyzed logs! (prev: {run_timestamp}, this: {line["timestamp"]}, diff: {diff})')
                 return instrumentation_data
 
         first_line = False
+        log_linenumber += 1
 
         if match := state_data_regex.search(line['message']):
             data = match.group('csv').split(',')
-            if match.group('version') is not None:
-                click.echo('Unsupported version for state data!')
-                continue
+            version = match.group('version')
+            if version is not None:
+                try:
+                    version = int(version)
+                    if version != 1:
+                        click.echo(f'Unsupported version for state data! {version}')
+                        continue
+                except ValueError:
+                    click.echo(f'Unsupported version for state data! {version}')
+                    continue
+            else:
+                version = 0
 
-            if len(data) != 4:
+            expected_fields = 4
+            if version == 1:
+                expected_fields = 5
+
+            if len(data) != expected_fields:
                 continue
 
             nf_data = instrumentation_data.get(line['domain'], {'time': {}, 'state_changes': {}})
             obj_data = nf_data['state_changes'].get(data[0], {'events': [], 'child_events': {}})
             if data[1] == "":
-                obj_data['events'].append({'event': data[2], 'timestamp': line['timestamp'].isoformat(), 'message': data[3]})
+                event = {'event': data[2], 'timestamp': line['timestamp'].isoformat(), 'message': data[3], 'linenumber': log_linenumber}
+                if version >= 1:
+                    event["function"] = data[4]
+                obj_data['events'].append(event)
             else:
                 child_events = obj_data['child_events'].get(data[1], {'events': []})
-                child_events['events'].append({'event': data[2], 'timestamp': line['timestamp'].isoformat(), 'message': data[3]})
+                event = {'event': data[2], 'timestamp': line['timestamp'].isoformat(), 'message': data[3], 'linenumber': log_linenumber}
+                if version >= 1:
+                    event["function"] = data[4]
+                child_events['events'].append(event)
 
                 obj_data['child_events'][data[1]] = child_events
 
@@ -105,16 +130,42 @@ def parse_instrumentation_messages(log_generator, instrumentation_data=None) -> 
             nf_data = instrumentation_data.get(line['domain'], {'time': {}, 'state_changes': {}})
             timings = nf_data['time'].get(data[0], [])
 
-            timings.append({'duration': float(data[1]), 'timestamp': line['timestamp'].isoformat()})
+            timings.append({'duration': float(data[1]), 'timestamp': line['timestamp'].isoformat(), 'linenumber': log_linenumber})
 
             nf_data['time'][data[0]] = timings
             instrumentation_data[line['domain']] = nf_data
+
+        elif match := timemarker_data_regex.search(line['message']):
+            data = match.group('csv').split(',')
+            if match.group('version') is not None:
+                click.echo('Unsupported version for state data!')
+                continue
+
+            if len(data) != 2:
+                continue
+
+            all_marker_data = timemarker.get(line['domain'], {})
+            marker_data = all_marker_data.get(data[0], [])
+
+            marker_data.append({'event': data[1], 'timestamp': line['timestamp'].isoformat(), 'linenumber': log_linenumber})
+
+            all_marker_data[data[0]] = marker_data
+            timemarker[line['domain']] = all_marker_data
+
+    if timemarker:
+        for nf in timemarker:
+            for function in timemarker[nf]:
+                if sum([1 for m in timemarker[nf][function] if m['event'] == "start"]) != sum([1 for m in timemarker[nf][function] if m['event'] == "stop"]) :
+                    print(f"Function {function} is missing a marker!")
+
+            if nf_data := instrumentation_data.get(nf):
+                nf_data['timemarker'] = timemarker[nf]
 
     return instrumentation_data
 
 
 @click.command()
-@click.option('--dump/--no-dump', default=False)
+@click.option('--dump', default=False, is_flag=True)
 @click.option('--run', type=str, required=False)
 @click.argument('logdir', type=click.Path(exists=True, file_okay=False))
 @click.argument('outdir', type=click.Path(exists=True, file_okay=False), required=False)
@@ -129,10 +180,10 @@ def main(dump, run, logdir, outdir=None):
     instrumentation_data = None
 
     if run is not None:
-        run = datetime.datetime.strptime(run, '%Y%m%d-%H%M%S')
+        run = datetime.datetime.strptime(run, '%Y%m%d-%H%M%S.%f')
 
     for logfile in logdir.glob("*.log"):
-        if "mongodb" in logfile.stem:
+        if "mongodb" in logfile.stem or "gnb" in logfile.stem or "ue" in logfile.stem:
             continue
 
         if dump:
@@ -143,7 +194,7 @@ def main(dump, run, logdir, outdir=None):
 
     if instrumentation_data is not None:
         with (outdir / f'instrumentation-data-{instrumentation_data["__run_timestamp"]}.json').open('w') as f:
-            json.dump(instrumentation_data, f)
+            json.dump(instrumentation_data, f, indent=2)
             click.echo(f'Wrote parsed state and time date to {f.name}')
 
 
